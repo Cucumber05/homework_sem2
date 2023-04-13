1dbf7af491f471c6153991c6d4b4fdc5d692301b
Cucumber05 <1132226163@pfur.ru>
2023-04-13 23:07:30 +0300

    input data

--- laba1304homework/1.c
+++ laba1304homework/1.c
@@ -4,8 +4,16 @@
 
 int main(){
     char word[20];
-    printf("Введите строку: ");
+    char symbol[1];
+    int k = 0;
+    printf("Введите строку (20): ");
     scanf('%s',word);
+    printf("Какой символ проверить?: ");
+    scanf('%s',symbol);
+    
+    for(int i; i<20; i++){
+
+    }
 
 
     return 0;
