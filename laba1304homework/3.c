ef3576b42e2c34b15f1127964d55d255fe5e697b
Cucumber05 <1132226163@pfur.ru>
2023-04-16 22:28:08 +0300

    add func

--- laba1304homework/3.c
+++ laba1304homework/3.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
+
+void print_array(int n, int a);
+void append_to_array();
+void insert_in_array();
 
 int main(){
 
-    return 0;
+}
+
+void print_array(int n, int a){
+    for (int i; i = 0; i<n; i++)
+        printf("%d ", a[i]);
+}
+void append_to_array(){
+    
+}
+void insert_in_array(){
+    
 }
