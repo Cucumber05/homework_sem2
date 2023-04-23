4f1c24ef06e94d7e6c51cbabe1abde9c8d7d89a7
Cucumber05 <1132226163@pfur.ru>
2023-04-23 08:35:54 +0300

    Bubble sort

--- 7laba/1.c
+++ 7laba/1.c
@@ -3,29 +3,62 @@
 
 int main()
 {
-    int a[100], n, i, j, position, swap, k;
+    int a[100], n, position, swap, k;
     printf("Enter number of elementsn");
     scanf("%d", &n);
     printf("Enter %d Numbersn", n);
 
     //Selection Sort
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
-        for(i = 0; i < n - 1; i++){
-            position=i;
-            for(j = i + 1; j < n; j++){
-                k++;
-                if(a[position] > a[j])
-                    position=j;
-                }
-                if(position != i)
-                {
+
+    for(int i = 0; i < n - 1; i++){
+        position = i;
+        for(int j = i + 1; j < n; j++){
+            k++;
+            if(a[position] > a[j])
+                position=j;
+            }
+            if(position != i){
                 swap=a[i];
                 a[i]=a[position];
                 a[position]=swap;
-                
-                }
+            }
     }
-    
+    printf("Selection Sorted Array:");
+    for(int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\nk=%d", k); //number of itarations
+    k=0;
+    swap=0;
+    n=0;
+
+    printf("Enter number of elementsn");
+    scanf("%d", &n);
+    printf("Enter %d Numbersn", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    //Bubble sort
+    for (int c = 0 ; c < n - 1; c++)
+    {
+        for (int d = 0 ; d < n - c - 1; d++)
+        {
+        if (a[d] > a[d+1])
+        {
+            swap = a[d];
+            a[d] = a[d+1];
+            a[d+1] = swap;
+            k++;
+        }
+        }
+    }
+
+    printf("\n Bubble Sorted list:\n");
+
+    for (int c = 0; c < n; c++)
+        printf("%d ", a[c]);
+    printf("\nk=%d", k); //number of itarations
+    return 0;
 
 }
