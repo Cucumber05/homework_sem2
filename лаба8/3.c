9872b5e1762aae71c26a89a0ae45d8a6c6d3a7c9
Cucumber05 <1132226163@pfur.ru>
2023-06-08 00:57:51 +0300

    end

--- лаба8/3.c
+++ лаба8/3.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            printf("пробел %d\n", c);
+        } else if (c == '\t') {
+            printf("\\t %d\n", c);
+        } else if (c == '\n') {
+            printf("\\n %d\n", c);
+        } else if (c < 32 || c > 126) {
+            printf("непечатаемый символ%d\n", c);
+        } else {
+            printf("%c %d\n", c, c);
+        }
+    }
+    return 0;
 }
