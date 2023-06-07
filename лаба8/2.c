0e4cca1f3f0d4b9731236f5db7b6038376afaaa7
Cucumber05 <1132226163@pfur.ru>
2023-06-08 00:42:37 +0300

    переменные

--- лаба8/2.c
+++ лаба8/2.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    
+    char source_file[100], target_file[100];
+    FILE *source, *target;
+    printf("Введите имя исходного файла: ");
+    scanf("%s", source_file);
+    printf("Введите имя итогового файла: ");
+    scanf("%s", target_file);
+    source = fopen("source_file.txt", "rb");
+    target = fopen("target_file.txt", "wb");
+
     return 0;
 }
